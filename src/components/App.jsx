768bb14494f3d92c3c7aaba6d74882de4c24a516
hitmen798 <124578265+hitmen798@users.noreply.github.com>
2023-12-13 13:19:58 +0100

    123

---
+++ src/components/App.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import style from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleAddNewContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  handleDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    const contactsName = contacts.map(contact => contact.name);
+
+    return (
+      <div>
+        <h1 className={style.title}>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.handleAddNewContact}
+          contactsName={contactsName}
+        />
+
+        <h2 className={style.title}>Contacts</h2>
+        <div className={style.contact_list_container}>
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList
+            visibleContacts={visibleContacts}
+            onDeleteContact={this.handleDeleteContact}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import style from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = { name: '', number: '' };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+
+    if (name === 'number' && !/^[0-9\s-+()]*$/.test(value)) {
+      alert('Введіть лише цифри, символи та пробіл!');
+      return;
+    }
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    const { name, number } = this.state;
+    event.preventDefault();
+
+    const matchName = this.props.contactsName.some(
+      contactName => name.toLowerCase() === contactName.toLowerCase()
+    );
+    if (matchName) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.props.onSubmit(newContact);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit} className={style.form}>
+        <label className={style.label}>
+          Name
+          <input
+            type="text"
+            name="name"
+            className={style.input}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+            value={name}
+            autoFocus
+          />
+        </label>
+
+        <label className={style.label}>
+          Number
+          <input
+            type="tel"
+            name="number"
+            className={style.input}
+            pattern="[0-9\s-+()]*"
+            title="Phone number must contain only digits, symbols (+, -, (, ), space)"
+            required
+            onChange={this.handleChange}
+            value={number}
+          />
+        </label>
+
+        <button type="submit" className={style.submit_btn}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  contactsName: PropTypes.arrayOf(PropTypes.string.isRequired),
+};
+
+export default ContactForm;
